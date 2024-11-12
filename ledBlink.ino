21a1cdf8caf0b4ddd43950e1e5784e0483d610e1
rochae03 <153113684+rochae03@users.noreply.github.com>
2024-11-12 14:24:38 -0500

    Update ledBlink.ino

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,9 +1,12 @@
-void setup() {
-  // put your setup code here, to run once:
 
+int ledPin = 24
+
+  void
+  setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT)
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(ledPin, HIGH);
 }
