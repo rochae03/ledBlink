93e5cc007ccde81007a7f65ee62cb3761e3ddf29
rochae03 <153113684+rochae03@users.noreply.github.com>
2024-11-12 14:49:48 -0500

    brightness

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,6 +1,4 @@
-
-
-int ledPins[] = { 24, 25, 26, 27 };
+int ledPins[4] = { 24, 25, 3, 5 };
 
 void setup() {
   Serial.begin(9600);
@@ -10,10 +8,12 @@ void setup() {
 }
 
 void loop() {
+  int potValue = analogRead(A14);  
+  int brightness = map(potValue, 0, 1023, 0, 255); 
   for (int i = 0; i < 4; i++) {
-    digitalWrite(ledPins[i], HIGH);
+    analogWrite(ledPins[i], brightness);  
     delay(500);
-    digitalWrite(ledPins[i], LOW);
+    analogWrite(ledPins[i], 0);  
     delay(500);
   }
 }
