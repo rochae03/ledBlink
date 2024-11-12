ff1c2c55ed6077d296fc95e3bedeaf546a1b1a31
rochae03 <153113684+rochae03@users.noreply.github.com>
2024-11-12 14:26:55 -0500

    added led

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,12 +1,16 @@
 
 int ledPin = 24
+int ledPin2 = 25
 
   void
   setup() {
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT)
+  pinMode(ledPin1, OUTPUT)
+  pinMode(ledPin2, OUTPUT)
+  
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin1, HIGH);
+  digitalWrite(ledPin2, HIGH);
 }
